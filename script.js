cb16457238a0ebd60863ea1678084fe928e3984e
Vynaa <118973846+VynaaValerie@users.noreply.github.com>
2024-02-20 14:54:12 +0700

    script.js

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbw4w5CNGwkej9UxeaZlY9v4AbO6w5YktbaHT9h_nleVogUXtj8Yg4f5wu6azPjm0vi_tQ/exec'
 const form = document.forms['submit-to-google-sheet']
-const btnSend =document.querySelector('.submit');
+const btnSend = document.querySelector('.submit');
 const load = document.querySelector('.loading');
-const buttons =document.querySelector('.btnSection');
-const button = document.querySelectorAll('.btnS')
+const buttons = document.querySelector('.btnSection');
+const button = document.querySelectorAll('.btnS');
 let article = document.querySelectorAll(".article");
-button[0].classList.add('bg-pink-400')
+button[0].classList.add('bg-pink-400');
 form.addEventListener('submit', e => {
-  e.preventDefault()
+  e.preventDefault();
   load.classList.toggle('hidden');
   btnSend.classList.toggle('hidden');
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => {
-        load.classList.toggle('hidden');
-        btnSend.classList.toggle('hidden');
-        Swal.fire({
+      load.classList.toggle('hidden');
+      btnSend.classList.toggle('hidden');
+      Swal.fire({
         title: 'Success!',
         text: 'Your message has been successfully sent.',
         icon: 'Success',
         confirmButtonText: 'Ok'
       });
     })
-    .catch(error => console.error('Error!', error.message))
-})
-let bar= document.getElementsByClassName('fa-bars')[0];
-let close=document.getElementsByClassName('close')[0];
-let navbar= document.getElementsByClassName('navbar')[0];
+    .catch(error => console.error('Error!', error.message));
+});
+let bar = document.getElementsByClassName('fa-bars')[0];
+let close = document.getElementsByClassName('close')[0];
+let navbar = document.getElementsByClassName('navbar')[0];
 
 function toggle() {
-    navbar.classList.toggle('hidden')
-    close.classList.toggle('hidden')
+  navbar.classList.toggle('hidden');
+  close.classList.toggle('hidden');
 }
+
 function toggle2() {
-    navbar.classList.toggle('hidden')
-    close.classList.toggle('hidden')
+  navbar.classList.toggle('hidden');
+  close.classList.toggle('hidden');
 }
+
 close.addEventListener('click', toggle);
 bar.addEventListener('click', toggle);
 document.getElementById('btn').addEventListener('click', function() {
@@ -52,25 +54,13 @@ buttons.addEventListener("click", function (e) {
       art.classList.add("hidden");
     });
     const element = document.getElementById(id);
-    console.log(element)
+    console.log(element);
     element.classList.remove("hidden");
-    console.log(element)
+    console.log(element);
   }
 });
+
 const cardItems = [
-  {
-    img1:"img/foodChoice1.png" ,
-    img2:"img/foodChoice2.png",
-    title1: "Food Choice",
-    miniTitle: "Web Development",
-    desc1:"This project is for people seeking specific recipes, offering a diverse database with filters for breakfast, lunch, dinner, and drinks. It also includes allergy information for a healthier cooking experience.",
-    desc2: "In this project, I've gained knowledge about working with APIs, fetching and filtering data.I've also learned how to implement pagination, all using JavaScript, without relying on any frameworks.This experience has significantly enhanced my JavaScript skills.",
-    btn1:"HTML",
-    btn2:"CSS",
-    btn3:"Javascript",
-    link:"https://food-choices.vercel.app/",
-    link2:"https://github.com/ManisaBachtiar/Food-Choices"
-  },
   {
     img1:"img/portofolio1.png" ,
     img2:"img/portofolio2.png",
@@ -81,33 +71,11 @@ const cardItems = [
     btn1:"HTML",
     btn2:"Tailwind",
     btn3:"Javascript",
-    link:"https://manisabachtiar.github.io/",
-    link2:"https://github.com/ManisaBachtiar/manisaBachtiar.github.io "
-  },
-  {img1:"img/course1.png" ,
-  img2:"img/course2.png",
-  title1: "English Course",
-  miniTitle: "UI/UX Design",
-  desc1:"I've meticulously put together a user-friendly interface and learning experience, seamlessly integrating course content with mentor guidance for an adaptable online learning adventure that maximizes student engagement and comprehension.",
-  desc2: "So, during this project, I delved into crafting impressive reviews and assembling a polished logo for the course icon. It was a hands-on journey, focused on enhancing aesthetics and effectively sharing information with a touch of sophistication.",
-  btn1:"Figma",
-  btn2:"Logo",
-  btn3:"UI/UX",
-  link:"https://dribbble.com/shots/17219512-English-Course",
-},
-{
-  img1:"img/marketPlace1.png" ,
-  img2:"img/marketPlace2.png",
-  title1: "Cat MarketPlace",
-  miniTitle: "UI/UX Design",
-  desc1:"I’m cooking up a UI/UX project to make life awesome for cat lovers. It's all about finding cats, snagging their favorite goodies and accessories, and throwing in some tips for a healthy kitty lifestyle. We're crafting a platform that's so user-friendly, it'll have cat enthusiasts feeling like pet care rockstars! ",
-  desc2: "In my first go at UI/UX, I got the hang of using Figma. I played around with designs, making sure they not only looked good but also had that extra charm to make them super appealing.",
-  btn1:"Design",
-  btn2:"Figma",
-  btn3:"UI/UX",
-  link:"https://dribbble.com/shots/17144625-Cats-Marketplace",
-}
-]
+    link:"https://vynaavalerie.github.io/pinaa/",
+    link2:"gh repo clone VynaaValerie/pinaa "
+  }
+];
+
 window.addEventListener("DOMContentLoaded", function () {
   displayCards(cardItems); // Memanggil fungsi untuk menampilkan semua kartu
 });
